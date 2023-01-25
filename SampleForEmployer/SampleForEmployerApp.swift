3cf539e3d425a8a110f5dee21522923bef62cd50
Sergey Glushchenko <glushchenko2003@gmail.com>
2023-01-25 16:12:32 +0800

    init commit

---
+++ SampleForEmployer/SampleForEmployerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleForEmployerApp.swift
+//  SampleForEmployer
+//
+//  Created by Sergey Glushchenko on 25.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SampleForEmployerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
