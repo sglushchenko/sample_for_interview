e3fff946b28235bfe4f4858b412ea66f27327c90
Sergey Glushchenko <glushchenko2003@gmail.com>
2023-01-25 18:29:53 +0800

    simple user view

--- SampleForEmployer/UserDetailsView.swift
+++ SampleForEmployer/UserDetailsView.swift
@@ -8,13 +8,15 @@
 import SwiftUI
 
 struct UserDetailsView: View {
+    let user: User
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Welcome to \(user.name)")
     }
 }
 
 struct UserDetailsView_Previews: PreviewProvider {
     static var previews: some View {
-        UserDetailsView()
+        UserDetailsView(user: User.sample)
     }
 }
